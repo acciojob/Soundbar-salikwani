73bb77fa81d2eebc9471dac46c947c440e4a826d
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-25 12:15:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+var play = document.querySelector(".btn");
+var flag = false;
+play.addEventListener("click", ()=> {
+	if(!flag) {
+		document.querySelector("audio").play();
+		flag = true;
+	}
+});
+var stop = document.querySelector(".stop");
+stop.addEventListener("click", ()=> {
+	if(flag) {
+		document.querySelector("audio").pause();
+		flag = false;
+	}
+});
